43261047e930d9eef91b602ea164143f91a58b92
Mark Kingsnorth <mark.kingsnorth@gmail.com>
2014-02-24 22:43:07 +0000

    simplified UI and made more generic

--- js/matrix.js
+++ js/matrix.js
@@ -4,13 +4,13 @@
 
 	function _MatrixView (_id) {
 
-		var matrixData;
-
-		var margin = {top: 80, right: 0, bottom: 10, left: 80},
+		var matrixData,
+			margin = {top: 80, right: 0, bottom: 10, left: 80},
 			width = 720,
-			height = 720;
-
-		var row, column, rowLine, colLine, rowText, colText;
+			height = 720,
+			x = d3.scale.ordinal().rangeBands([0, width]), 
+			y = d3.scale.ordinal().rangeBands([0, height]), 
+			z = d3.scale.linear().domain([0, 4]).clamp(true);
 
 		var svgElem = d3.select(_id).append("svg")
 		.style("margin-left", -margin.left + "px");
@@ -18,61 +18,10 @@
 		var svg = svgElem.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-		var x = d3.scale.ordinal().rangeBands([0, width]), 
-			y = d3.scale.ordinal().rangeBands([0, height]), 
-			z = d3.scale.linear().domain([0, 4]).clamp(true);
-
 		var rect = svg.append("rect")
 			  .attr("class", "background");
 
-		var redraw = function() {
-			svgElem.attr("width", width + margin.left + margin.right);
-			svgElem.attr("height", height + margin.top + margin.bottom);
-			rect.attr("width", width);
-			rect.attr("height", height);
-			x.rangeBands([0, width]);
-			y.rangeBands([0, height]);
-			var xRangeBand = x.rangeBand();
-			var yRangeBand = y.rangeBand();
-			if (isFinite(xRangeBand) && isFinite(yRangeBand)) {
-				row.attr("transform", function(d, i) { 
-			  		return "translate(0," + y(i) + ")"; 
-			  	});
-				rowText.attr("y", yRangeBand / 2);
-				rowText.attr("font-size", yRangeBand + "px");
-				rowLine.attr("x2", height);
-				column.attr("transform", function(d, i) { 
-			  		return "translate(" + x(i) + ")rotate(-90)"; 
-			  	});
-				colText.attr("y", xRangeBand / 2);
-				colText.attr("font-size", xRangeBand + "px");
-				colLine.attr("x1", -width);
-				d3.selectAll(".cell")
-					.attr("width", xRangeBand)
-					.attr("height", yRangeBand)
-					.attr("x", function(d) { return x(d.x); })
-			}
-		};
-
-		this.size = function(_size) {
-			if (_size) {
-				width = _size.width;
-				height = _size.height;
-				return this;	
-			} else {
-				return {
-					width: width,
-					height: height
-				};
-			}
-		};
-
-		this.data = function(_matrixData) {
-			matrixData = _matrixData;
-			return this;
-		};
-
-		this.render = function() {
+		var render = function() {
 			if (!matrixData) {
 				return;
 			}
@@ -86,9 +35,9 @@
 			  	matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0, nodeA: nodes[j].name, nodeB: nodes[i].name}; });
 			  	//add unique key that defines this first level matrix entry.
 			  	matrix[i].key = function() {
-			  		var values = matrix[i].map(function(d) {return d.z;});
-			  		console.log(Math.random() + nodes[i].name + "(" + values.join() + ")");
-			  		return Math.random() + nodes[i].name + "(" + values.join() + ")";
+			  		var zValues = matrix[i].map(function(d) {return d.z;});
+			  		var nodeAValues = matrix[i].map(function(d) {return d.nodeA;});
+			  		return nodes[i].name + "(" + nodeAValues.join() + ")(" + zValues.join() + ")";
 			  	};
 			  	//matrix row/column name
 			  	matrix[i].labelName = nodes[i].name;
@@ -112,24 +61,25 @@
 			  y.domain(orders.name);
 
 			  var rowData = svg.selectAll(".row")
-			  .data(matrix, function(d,i) { 
+			  .data(matrix, function(d,i) {
 			  	return d.key();
 			  });
 
 			  rowData.exit().remove();
 
-			  row = rowData.enter().append("g")
+			  var row = rowData.enter().append("g")
 			  .attr("class", "row")
 			  .each(renderRow);
 
-			  rowLine = row.append("line");
+			  var rowLine = row.append("line")
+			  				   .attr("class", "rowLine");
 
-			  rowText = row.append("text")
+			  var rowText = row.append("text")
 				  .attr("x", -6)
 				  .attr("dy", ".32em")
 				  .attr("text-anchor", "end")
 				  .attr("class", function(d) {
-				  	return d.labelName+ "rowName";;
+				  	return d.labelName+ "rowName rowText";;
 				  })
 				  .text(function(d, i) { 
 				  	return d.labelName; 
@@ -142,17 +92,18 @@
 
 			  columnData.exit().remove();
 
-			  column = columnData.enter().append("g")
+			  var column = columnData.enter().append("g")
 			  .attr("class", "column");
 
-			  colLine = column.append("line");
+			  var colLine = column.append("line")
+			  					  .attr("class", "colLine");
 
-			  colText = column.append("text")
+			  var colText = column.append("text")
 			  .attr("x", 6)
 			  .attr("dy", ".32em")
 			  .attr("text-anchor", "start")
 			  .attr("class", function(d) {
-			  	return d.labelName + "colName";
+			  	return d.labelName + "colName colText";
 			  })
 			  .text(function(d, i) { 
 			  	return d.labelName; 
@@ -187,6 +138,65 @@
 			  }
 			  redraw();
 
+		};
+
+		var redraw = function() {
+			svgElem.attr("width", width + margin.left + margin.right);
+			svgElem.attr("height", height + margin.top + margin.bottom);
+			rect.attr("width", width);
+			rect.attr("height", height);
+			x.rangeBands([0, width]);
+			y.rangeBands([0, height]);
+			var xRangeBand = x.rangeBand();
+			var yRangeBand = y.rangeBand();
+			if (isFinite(xRangeBand) && isFinite(yRangeBand)) {
+				var row = d3.selectAll(".row")
+				row.attr("transform", function(d, i) { 
+			  		return "translate(0," + y(i) + ")"; 
+			  	});
+			  	var rowText = d3.selectAll(".rowText");
+				rowText.attr("y", yRangeBand / 2);
+				var rowFontSize = yRangeBand > 12 ? 12 : yRangeBand;
+				rowText.attr("font-size", rowFontSize + "px");
+				var rowLine = d3.selectAll(".rowLine");
+				rowLine.attr("x2", height);
+				
+				var column = d3.selectAll(".column");
+				column.attr("transform", function(d, i) { 
+			  		return "translate(" + x(i) + ")rotate(-90)"; 
+			  	});
+			  	var colText = d3.selectAll(".colText");
+				colText.attr("y", xRangeBand / 2);
+				var colFontSize = xRangeBand > 12 ? 12 : xRangeBand;
+				colText.attr("font-size", colFontSize + "px");
+				var colLine = d3.selectAll(".colLine");
+				colLine.attr("x1", -width);
+
+				d3.selectAll(".cell")
+					.attr("width", xRangeBand)
+					.attr("height", yRangeBand)
+					.attr("x", function(d) { return x(d.x); });
+			}
+		};
+
+		this.size = function(_size) {
+			if (_size) {
+				width = _size.width;
+				height = _size.height;
+				redraw();
+				return this;	
+			} else {
+				return {
+					width: width,
+					height: height
+				};
+			}
+		};
+
+		this.data = function(_matrixData) {
+			matrixData = _matrixData;
+			render();
+			return this;
 		};
 
 	}
